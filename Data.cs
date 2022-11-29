1eb50fc22cf216eb047072502ece5316f9c27d66
SkyRez <makkoar@gmail.com>
2022-11-29 15:01:09 +0300

    Fix

--- Data.cs
+++ Data.cs
@@ -2,10 +2,10 @@
 {
     public static class Data
     {
-        public static Theme BlackTheme = new(ConsoleColor.Black, ConsoleColor.White);
-        public static Theme GreenTheme = new(ConsoleColor.Green, ConsoleColor.Black);
-        public static Theme YellowTheme = new(ConsoleColor.Yellow, ConsoleColor.DarkBlue);
-        public static Theme RedTheme = new(ConsoleColor.Red, ConsoleColor.White);
-        public static Theme BlueTheme = new(ConsoleColor.Blue, ConsoleColor.Yellow);
+        public static Theme BlackTheme { get; } = new(ConsoleColor.Black, ConsoleColor.White);
+        public static Theme GreenTheme { get; } = new(ConsoleColor.Green, ConsoleColor.Black);
+        public static Theme YellowTheme { get; } = new(ConsoleColor.Yellow, ConsoleColor.DarkBlue);
+        public static Theme RedTheme { get; } = new(ConsoleColor.Red, ConsoleColor.White);
+        public static Theme BlueTheme { get; } = new(ConsoleColor.Blue, ConsoleColor.Yellow);
     }
 }
