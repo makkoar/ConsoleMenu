5bb744cc701d5d133defb5a3e3e5b6010378ab61
makkaor <32832414+makkoar@users.noreply.github.com>
2023-01-13 04:31:01 +0300

    Add files via upload

---
+++ ConsoleMenu/Data.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleMenu
+{
+    public static class Data
+    {
+        public static Theme BlackTheme { get; } = new(ConsoleColor.Black, ConsoleColor.White);
+        public static Theme GreenTheme { get; } = new(ConsoleColor.Green, ConsoleColor.Black);
+        public static Theme YellowTheme { get; } = new(ConsoleColor.Yellow, ConsoleColor.DarkBlue);
+        public static Theme RedTheme { get; } = new(ConsoleColor.Red, ConsoleColor.White);
+        public static Theme BlueTheme { get; } = new(ConsoleColor.Blue, ConsoleColor.Yellow);
+    }
+}
