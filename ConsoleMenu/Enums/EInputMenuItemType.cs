aa699fef2b84ebf62c6fe36b146a0db8670f348b
Kolesnikov_MI <makkoar@gmail.com>
2025-06-19 10:06:12 +0300

    Добавлено перечисление EInputMenuItemType
    В файл EInputMenuItemType.cs добавлено перечисление EInputMenuItemType, которое включает типы элементов меню ввода: строка, целое число, число с плавающей запятой, двойное число, десятичное число и логическое значение. Также добавлены комментарии для каждого типа.

---
+++ ConsoleMenu/Enums/EInputMenuItemType.cs
@@ -0,0 +1,18 @@
+namespace ConsoleMenu.Enums;
+
+/// <summary>Перечисление возможных типов значения для InputMenuItem.</summary>
+public enum EInputMenuItemType
+{
+    /// <summary>Строковое значение.</summary>
+    String,
+    /// <summary>Целое число (int).</summary>
+    Int,
+    /// <summary>Число с плавающей точкой (float).</summary>
+    Float,
+    /// <summary>Число с плавающей точкой двойной точности (double).</summary>
+    Double,
+    /// <summary>Десятичное число (decimal).</summary>
+    Decimal,
+    /// <summary>Логическое значение (bool).</summary>
+    Bool
+}
